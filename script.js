ef64b913046f36daccc0984c7bd5b02eb1ee9d1e
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-21 21:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const playSound = (event) => {
+  const targetFile = event.target.innerText;
+  const soundElement = document.getElementById("soundSource");
+  soundElement.setAttribute("src", `./sounds/${targetFile}.mp3`);
+  soundElement.load();
+  soundElement.play();
+};
+
+const stopSound=()=>{
+   const soundElement = document.getElementById("soundSource");
+  soundElement.pause();
+}
